66f9e29dd7aa075fe30704adef5ae3a19d5c78ee
Alain <alain.andres@ext.aqualogybs.net>
2020-11-22 14:48:35 +0100

    Solved PROTOCOL_CONNECTION_LOST

--- config/db.config.js
+++ config/db.config.js
@@ -1,16 +1,34 @@
 const mysql = require('mysql');
 
 //local mysql db connection
-const dbConn = mysql.createConnection({
+const db_config = {
   host     : 'localhost',
   user     : 'root',
   password : 'root',
   database : 'node_mysql_crud_db'
-});
+};
 
-dbConn.connect(function(err) {
-  if (err) throw err;
-  console.log("Database Connected!");
-});
+var connection;
 
-module.exports = dbConn;
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config);
+
+  connection.connect(function(err) {
+    if(err) {                                     
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000);
+    }
+  });
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
+
+module.exports = connection;
